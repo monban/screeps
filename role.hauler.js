2ee1c26b481697fc3499553b265484c369d14e22
Ian Flynn <himitsu.monban@gmail.com>
2016-07-20 00:44:16 -0400

    build roads and stuff!

--- role.hauler.js
+++ role.hauler.js
@@ -1,8 +1,11 @@
 module.exports = {
   run: function(creep) {
+    if ((creep.room.lookForAt(LOOK_STRUCTURES, creep).length == 0) && (creep.room.lookForAt(LOOK_CONSTRUCTION_SITES, creep).length == 0)) {
+      creep.room.createConstructionSite(creep, STRUCTURE_ROAD);
+    }
     if (_.sum(creep.carry) == creep.carryCapacity) {
       var storages = creep.room.find(FIND_MY_STRUCTURES, {
-        filter: i => i.energy < i.energyCapacity
+        filter: i => i.energy < (i.energyCapacity / 2)
       });
       storages.sort(function(a,b) {
         var ast = a.structureType;
