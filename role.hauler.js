6e0a2e458d1e550c8f491306f6ce2f08d6e13f28
Ian Flynn <himitsu.monban@gmail.com>
2016-07-19 23:04:34 -0400

    add some prioritization to energy distribution

--- role.hauler.js
+++ role.hauler.js
@@ -4,8 +4,17 @@ module.exports = {
       var storages = creep.room.find(FIND_MY_STRUCTURES, {
         filter: i => i.energy < i.energyCapacity
       });
+      storages.sort(function(a,b) {
+        var ast = a.structureType;
+        var bst = b.structureType;
+        if (ast == bst)
+          return 0;
+        if (ast == STRUCTURE_TOWER) // Give towers first priority on energy
+          return -1;
+        return 1;
+      });
       var target = storages[0];
-      target = target || Game.spawns['Spawn1'];
+      target = target || creep.room.storage;
       if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(target);
       }
