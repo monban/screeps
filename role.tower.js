e75d72a8e455db8eead88434656a7bac9e5b8867
Ian Flynn <himitsu.monban@gmail.com>
2016-07-20 00:20:43 -0400

    towers heal roads (with rather ugly memory management)

--- role.tower.js
+++ role.tower.js
@@ -1,8 +1,37 @@
+"use strict";
+
+function getNextRepairTarget(tower) {
+  const roads = tower.room.find(FIND_STRUCTURES, {
+    filter: i => i.structureType == STRUCTURE_ROAD && i.hits < i.hitsMax
+  });
+  if (roads.length != 0) {
+    Memory.towers[tower.id].repairTarget = roads[0].id;
+  }
+}
+
+function setupMemory(tower) {
+  if (!Memory.towers) {
+    Memory.towers = new Object;
+  }
+  if (!Memory.towers[tower.id]) {
+    Memory.towers[tower.id] = new Object;
+  }
+}
 module.exports = {
   run: function(tower) {
+    if (!Memory.towers || !Memory.towers[tower.id]) {
+      setupMemory(tower);
+    }
     var hostiles = tower.room.find(FIND_HOSTILE_CREEPS);
-    if (hostiles) {
+    if (hostiles.length > 0) {
       tower.attack(hostiles[0]);
+    } else {
+      const target = Game.getObjectById(Memory.towers[tower.id].repairTarget);
+      if (target && target.hits < target.hitsMax) {
+        tower.repair(target);
+      } else {
+        getNextRepairTarget(tower);
+      }
     }
   }
 }
