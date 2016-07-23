1be1e95b29b098af10da29c1dbe772a5c35208a1
Ian Flynn <himitsu.monban@gmail.com>
2016-07-23 03:16:58 -0400

    much improved harvester logic

--- role.harvester.js
+++ role.harvester.js
@@ -1,38 +1,58 @@
 function findOrRememberSource(creep)
 {
-//    if (!Memory.source_claims) {
-//        Memory.source_claims = new Array();
-//    }
-//    if (creep.memory.source) {
-//        return Game.getObjectById(creep.memory.source);
-//    } else {
-//        var sources = creep.room.find(FIND_SOURCES);
-//        for (var i=0; i<sources.length; i++) {
-//            var source = sources[i];
-//            if (Memory.source_claims[source.id])
-//                continue;
-//            creep.memory.source = source.id;
-//            Memory.source_claims[source.id] = true;
-//        }
-//    }
-    return creep.room.find(FIND_SOURCES)[0];
+  //    if (!Memory.source_claims) {
+  //        Memory.source_claims = new Array();
+  //    }
+  //    if (creep.memory.source) {
+  //        return Game.getObjectById(creep.memory.source);
+  //    } else {
+  //        var sources = creep.room.find(FIND_SOURCES);
+  //        for (var i=0; i<sources.length; i++) {
+  //            var source = sources[i];
+  //            if (Memory.source_claims[source.id])
+  //                continue;
+  //            creep.memory.source = source.id;
+  //            Memory.source_claims[source.id] = true;
+  //        }
+  //    }
+  return creep.room.find(FIND_SOURCES)[0];
+}
+
+// This function is pretty ugly
+function findMySpot(creep)
+{
+  const pos = creep.room.find(FIND_SOURCES)[0].pos;
+  const area = creep.room.lookAtArea(pos.y-1, pos.x-1, pos.y+1, pos.x+1, true);
+  const spots = _.groupBy(area, i => [i.x, i.y]);
+  const openSpots = _.filter(spots, i => _.find(i, j => j.structure && j.structure.structureType == STRUCTURE_CONTAINER) && !_.find(i, j => j.creep));
+  if (openSpots.length != 0) {
+    creep.memory.spot = {x: openSpots[0][0].x, y: openSpots[0][0].y};
+  }
 }
 
 var roleHarvester = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        var source = findOrRememberSource(creep);
-        if (creep.ticksToLive == 1) {
-            Memory.source_claims[source.id] = false;
-            creep.suicide();
-        }
+  /** @param {Creep} creep **/
+  run: function(creep) {
+    // Do we even have a remembered spot?
+    if (!creep.memory.spot) {
+      findMySpot(creep);
+    }
 
-        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(source);
-        }
-	},
-	bodyparts: [MOVE, WORK, WORK]
+    // Is someone already in our spot?
+    if (creep.room.lookForAt(LOOK_CREEPS, creep.memory.spot.x, creep.memory.spot.y)) {
+      // You're standing in my spot Sir
+      findMySpot(creep);
+    }
+    // Are we at our spot?
+    if (creep.pos.x == creep.memory.spot.x && creep.pos.y == creep.memory.spot.y) {
+      var source = findOrRememberSource(creep);
+      creep.harvest(source);
+    } else {
+      creep.moveTo(creep.memory.spot.x, creep.memory.spot.y);
+    }
+  },
+  bodyparts: [MOVE, WORK, WORK]
 };
 
 module.exports = roleHarvester;
