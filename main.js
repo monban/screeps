4c8a030de9c4e6f5698b81905a04e27146eadef1
Ian Flynn <himitsu.monban@gmail.com>
2017-08-14 05:41:14 -0400

    cleanup profiling code we aren't using right now

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 "use strict";
 const utils = require('utils');
-//const profiler = require('screeps-profiler');
 const controllerRoom = require('controller.room');
 
-//profiler.enable();
 module.exports.loop = function() {
-  //  profiler.wrap(function() {
-    // Every 100 ticks
-    if (Game.time % 100 == 0) {
-      // Clean up memory
-      utils.memory.clean();
-    }
-    // Run the room controller on each room in turn
-    _.forEach(Game.rooms, controllerRoom);
-  };
-//);
+  // Every 100 ticks
+  if (Game.time % 100 == 0) {
+    // Clean up memory
+    utils.memory.clean();
+  }
+  // Run the room controller on each room in turn
+  _.forEach(Game.rooms, controllerRoom);
+};
 
