5dae202ffceae58988178e12dcb313efb75b6984
Ian Flynn <himitsu.monban@gmail.com>
2017-08-14 22:22:30 -0400

    removed password from gruntfile (don't bother, already changed it)

--- gruntfile.js
+++ gruntfile.js
@@ -1,24 +1,19 @@
 module.exports = function(grunt) {
+  const config = require('./.screeps.json');
+  let options = {
+    branch: grunt.option('branch') || config.branch,
+    email: grunt.option('email') || config.email,
+    password: grunt.option('password') || config.password,
+    ptr: grunt.option('ptr') ? true : config.ptr
+  };
 
   grunt.loadNpmTasks('grunt-screeps');
 
   grunt.initConfig({
     screeps: {
-      options: {
-        email: 'himitsu.monban@gmail.com',
-        password: 'VYCVzzM7uKQgtRcL',
-        branch: 'default',
-        ptr: false
-      },
+      options: options,
       dist: {
-        files: [
-          {
-            expand: true,
-            cwd: 'dist/',
-            src: ['**/*.js'],
-            flatten: true
-          }
-        ]
+        src: ['dist/*.js']
       }
     }
   });
